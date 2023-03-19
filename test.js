b44b99c49d4da6f75404ec33a918ff884609ef69
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-03-18 20:19:35 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 // TODOs:
 
+
 //***/ General test
 // - Add a test for the "debug" option when it is true
+// - Add a test to check the GitHub API works and is enabled
 
 //***/ Since and last date tests
 // - Add a test for the "since" option when the date is empty
