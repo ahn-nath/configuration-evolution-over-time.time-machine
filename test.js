b46179206caa0fa757fa9633f1f847e64b8d885b
Nathaly Toledo <nathaly12toledo@gmail.com>
2023-03-18 20:00:43 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+// TODOs:
+
+//***/ General test
+// - Add a test for the "debug" option when it is true
+
+//***/ Since and last date tests
+// - Add a test for the "since" option when the date is empty
+// - Add a test for the "since" option when the date is not empty
+// - Add a test for the "since" option when the date is not empty and the date is in the future or in the wrong format
+
+//***/ Commits tests
+// - Add a test for an invalid line in a commit
+// - Add test for when the file already exists and the last date is expected to be valid
